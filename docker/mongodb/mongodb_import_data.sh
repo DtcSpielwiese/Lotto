8bdf43ac96535398c14445f9182e6c3fc4abac14
Silvio <silvio.rebentisch@msg.group>
2018-03-19 15:45:45 +0100

    Regionen, Versicherungen und Wertebereiche hinzugefügt

--- docker/mongodb/mongodb_import_data.sh
+++ docker/mongodb/mongodb_import_data.sh
@@ -10,9 +10,34 @@ done
 
 echo "=> Import DATA user '${MONGODB_USER}' with password '${MONGODB_PASS}' in MongoDB '${MONGODB_DATABASE}'"
 
-mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --eval "db.getCollection('neos').remove({});"
-echo "=> Collection neos removed"
+echo "=> remove Collection neo_praemien"
+mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --eval "db.getCollection('neo_praemien').remove({});"
+echo "=> Collection neo_praemien removed"
 
-mongoimport --db $MONGODB_DATABASE --collection neos --username $MONGODB_USER --password "$MONGODB_PASS" --type csv --headerline --file /Praemien_CH.csv
+echo "=> import Praemien_CH.csv"
+mongoimport --db $MONGODB_DATABASE --collection neo_praemien --username $MONGODB_USER --password "$MONGODB_PASS" --type csv --headerline --file /Praemien_CH.csv
 echo "=> Praemien_CH.csv imported"
 
+echo "=> remove Collection neo_wertebereiche"
+mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --eval "db.getCollection('neo_wertebereiche').remove({});"
+echo "=> Collection neo_wertebereiche removed"
+
+echo "=> import Wertebereiche_CH.csv"
+mongoimport --db $MONGODB_DATABASE --collection neo_wertebereiche --username $MONGODB_USER --password "$MONGODB_PASS" --type csv --headerline --file /Wertebereiche_CH.csv
+echo "=> Wertebereiche_CH.csv imported"
+
+echo "=> remove Collection neo_regionen"
+mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --eval "db.getCollection('neo_regionen').remove({});"
+echo "=> Collection neo_regionen removed"
+
+echo "=> import Regionen_CH.csv"
+mongoimport --db $MONGODB_DATABASE --collection neo_regionen --username $MONGODB_USER --password "$MONGODB_PASS" --type csv --headerline --file /Regionen_CH.csv
+echo "=> Regionen_CH.csv imported"
+
+echo "=> remove Collection neo_versicherungen"
+mongo $MONGODB_DATABASE -u $MONGODB_USER -p $MONGODB_PASS --eval "db.getCollection('neo_versicherungen').remove({});"
+echo "=> Collection neo_versicherungen removed"
+
+echo "=> import Versicherungen_CH.csv"
+mongoimport --db $MONGODB_DATABASE --collection neo_versicherungen --username $MONGODB_USER --password "$MONGODB_PASS" --type csv --headerline --file /Versicherungen_CH.csv
+echo "=> Versicherungen_CH.csv imported"
